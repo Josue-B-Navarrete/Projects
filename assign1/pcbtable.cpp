dea0c69b96b9e3f7fc1ec6a893dd47c57063c42c
Josue-B-Navarrete <90738503+Josue-B-Navarrete@users.noreply.github.com>
2025-02-04 10:25:50 -0800

    Add files via upload

---
+++ assign1/pcbtable.cpp
@@ -0,0 +1,65 @@
+/**
+ * Assignment 1: priority queue of processes
+ * @file pcbtable.h
+ * @author Xahn Millsape and Josue Navarrete
+ * @brief This is the implementation file for the PCBTable class.
+ * //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+ * // Remember to add sufficient comments to your code
+ */
+
+#include "pcbtable.h"
+
+/**
+ * @brief Construct a new PCBTable object of the given size (number of PCBs)
+ *
+ * @param size: the capacity of the PCBTable
+ */
+PCBTable::PCBTable(int size) {
+    table.resize(size, nullptr);
+}
+
+/**
+ * @brief Destroy the PCBTable object. Make sure to delete all the PCBs in the table.
+ *
+ */
+PCBTable::~PCBTable() {
+   // Delete all the PCBs in the table
+   // Delete all PCB objects stored in the vector
+   for (PCB* pcb : table) {
+        if (pcb != nullptr) {
+            delete pcb;
+        }
+    }
+}
+
+/**
+ * @brief Get the PCB at index "idx" of the PCBTable.
+ *
+ * @param idx: the index of the PCB to get
+ * @return PCB*: pointer to the PCB at index "idx"
+ */
+PCB* PCBTable::getPCB(unsigned int idx) {
+    if (idx >= table.size()) {
+        return nullptr;
+    }
+    return table[idx];
+}
+
+/**
+ * @brief Add a PCB pointer to the PCBTable at index idx.
+ *
+ * @param pcb: the PCB to add
+ */
+void PCBTable::addPCB(PCB *pcb, unsigned int idx) {
+    // Add a PCB pointer to the PCBTable at index idx.
+    // Check if index is within bounds
+    if (idx >= table.size()) {
+        return;
+    }
+    // If there's already a PCB at this index, delete it first
+    if (table[idx] != nullptr) {
+        delete table[idx];
+    }
+    // Add the new PCB
+    table[idx] = pcb;
+}
---
+++ assign1/readyqueue.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include "readyqueue.h"
+
+using namespace std;
+
+// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+// Remember to add sufficient comments to your code
+
+/**
+ * @brief Constructor for the ReadyQueue class.
+ */
+ ReadyQueue::ReadyQueue()  {
+     capacity = 100;  
+     currentSize = 0;
+     heap = new PCB*[capacity];
+ }
+
+/**
+ *@brief Destructor
+*/
+ReadyQueue::~ReadyQueue() {
+    delete heap;
+}
+
+/**
+ * @brief Add a PCB representing a process into the ready queue.
+ *
+ * @param pcbPtr: the pointer to the PCB to be added
+ */
+void ReadyQueue::addPCB(PCB *pcbPtr) {
+    // when adding, change its state to READY.
+    if (currentSize >= capacity) {
+        return;
+    }
+    // set state to READY and add to heap
+    pcbPtr->setState(ProcState::READY);
+    heap[currentSize] = pcbPtr;
+    heapifyUp(currentSize);
+    currentSize++;
+}
+
+/**
+ * @brief Remove and return the PCB with the highest priority from the queue
+ *
+ * @return PCB*: the pointer to the PCB with the highest priority
+ */
+PCB* ReadyQueue::removePCB() {
+    // when removing, change its state to RUNNING.
+        if (currentSize == 0) {
+        return nullptr;
+    }
+
+    // get the highest priority PCB
+    PCB* highestPriorityPCB = heap[0];
+    highestPriorityPCB->setState(ProcState::RUNNING);
+
+    // replace root with last element and heapify down
+    heap[0] = heap[currentSize - 1];
+    currentSize--;
+    
+    if (currentSize > 0) {
+        heapifyDown(0);
+    }
+
+    return highestPriorityPCB;
+}
+
+/**
+ * @brief Returns the number of elements in the queue.
+ *
+ * @return int: the number of PCBs in the queue
+ */
+int ReadyQueue::size() {
+    return currentSize;
+}
+
+/**
+ * @brief Display the PCBs in the queue.
+ */
+void ReadyQueue::displayAll() {
+    for (int i = 0; i < currentSize; i++) {
+        heap[i]->display();
+    }
+}
+
+/**
+ * @brief Helper function to maintain heap property upward
+ * @param Index The index of the node to start heapifying upward
+ */
+void ReadyQueue::heapifyUp(int index) {
+    // while not at the root
+    while (index > 0) {
+        // calculate parent index
+        int parent = (index - 1) / 2;
+        // if current node has a higher priority than parent, SWAP
+        if (heap[index]->getPriority() > heap[parent]->getPriority()) {
+            swap(index, parent);
+            // store parents index
+            index = parent;
+        } else {
+            break;
+        }
+    }
+}
+
+/**
+ * @brief Helper function to maintain heap property downward
+ * @param Index The index of the node to start heapifying upward
+ */
+void ReadyQueue::heapifyDown(int index) {
+    while (true) {
+        // set current node as largest
+        int maxIndex = index;
+        // childern indices
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        // check if childern exist and has higher prioroty
+        if (left < currentSize && 
+            heap[left]->getPriority() > heap[maxIndex]->getPriority()) {
+            maxIndex = left;
+        }
+        if (right < currentSize && 
+            heap[right]->getPriority() > heap[maxIndex]->getPriority()) {
+            maxIndex = right;
+        }
+
+        // if the largest is not the current node, SWAP
+        if (maxIndex != index) {
+            swap(index, maxIndex);
+            index = maxIndex;
+        } else {
+            break;
+        }
+    }
+}
+
+/**
+ * @brief Helper function to swap two elements in the heap
+ * @param i The index of the first element to swap
+ * @param j The index of the second element to swap
+ */
+void ReadyQueue::swap(int i, int j) {
+    // temp
+    PCB* temp = heap[i];
+    // swap
+    heap[i] = heap[j];
+    heap[j] = temp;
+}
