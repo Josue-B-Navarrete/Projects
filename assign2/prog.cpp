03bf53324343e503e2caf72ab251b39250a3fe01
Josue-B-Navarrete <90738503+Josue-B-Navarrete@users.noreply.github.com>
2025-02-25 10:24:08 -0800

    updated prog.cpp

--- assign2/prog.cpp
+++ assign2/prog.cpp
@@ -1,94 +1,154 @@
-
 /**
- * Assignment 2: Simple UNIX Shell
- * @file pcbtable.h
- * @author ??? (TODO: your name)
- * @brief This is the main function of a simple UNIX Shell. You may add additional functions in this file for your implementation
- * @version 0.1
+ * Simple UNIX Shell Implementation
+ * Following original parameter structure
  */
-// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
-
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
 #include <fcntl.h>
 #include <cstring>
-#include <unistd.h>
 #include <sys/wait.h>
-
 using namespace std;
 
 #define MAX_LINE 80 // The maximum length command
 
+// Global variables for history feature
+char last_command[MAX_LINE] = {0};
+bool has_history = false;
+
 /**
- * @brief parse out the command and arguments from the input command separated by spaces
- *
- * @param command
- * @param args
- * @return int
+ * Parse command into tokens
+ * @param command Input command string
+ * @param args Array to store command arguments
+ * @return Number of arguments
  */
-int parse_command(char command[], char *args[])
-{
-    // TODO: implement this function
-    char *ptr = strtok(command, " ");
+int parse_command(char command[], char *args[]) {
     int i = 0;
-    while(ptr != NULL) {
-        args[i] = ptr;
-        ptr = strtok(NULL, " ");
+    char *token;
+    
+    // Remove newline if present
+    size_t len = strlen(command);
+    if (len > 0 && command[len-1] == '\n') {
+        command[len-1] = '\0';
+    }
+    
+    // Parse the command using strtok
+    token = strtok(command, " ");
+    while (token != NULL && i < MAX_LINE/2) {
+        args[i] = token;
+        token = strtok(NULL, " ");
         i++;
     }
-    return i + 1;
+    
+    args[i] = NULL;  // Null terminate the argument list
+    return i;
 }
 
-// TODO: Add additional functions if you need
-
 /**
- * @brief The main function of a simple UNIX Shell. You may add additional functions in this file for your implementation
- * @param argc The number of arguments
- * @param argv The array of arguments
- * @return The exit status of the program
+ * Main function implementing the shell
  */
-int main(int argc, char *argv[])
-{
-    char command[MAX_LINE];       // the command that was entered
-    char *args[MAX_LINE / 2 + 1]; // hold parsed out command line arguments
-    int should_run = 1;           /* flag to determine when to exit program */
-
-    // TODO: Add additional variables for the implementation.
-
-    while (should_run)
-    {
+int main(void) {
+    char command[MAX_LINE];       // Command entered
+    char *args[MAX_LINE/2 + 1];   // Command line arguments
+    int should_run = 1;           // Flag to determine when to exit program
+    
+    while (should_run) {
         printf("osh>");
         fflush(stdout);
-        // Read the input command
-        fgets(command, MAX_LINE, stdin);
-        // Parse the input command
-        int num_args = parse_command(command, args);
-
-        int rc = fork();
-        if(rc < 0) {
-            //fork failed
-            fprint(stderr, "Fork failed\n");
-            exit(1); //should_run == 0 (?)
-        } else if(rc == 0) {
-            //child process
-            args[num_args]; //unsure if this is right
-            //need an if statment to check for a '&'
-            //if there is one, don't wait
-            execvp(args[0], args);
-            printf("This shouldn't print\n");
-        } else {
-            int wc = wait(NULL);
+        
+        // Read the command
+        if (!fgets(command, MAX_LINE, stdin)) {
+            break;
         }
-
-        // TODO: Add your code for the implementation
-        /**
-         * After reading user input, the steps are:
-         * (1) fork a child process using fork()
-         * (2) the child process will invoke execvp()
-         * (3) parent will invoke wait() unless command included &
-         */
+        
+        // Handle history (!!) command
+        if (strcmp(command, "!!\n") == 0) {
+            if (!has_history) {
+                printf("No commands in history.\n");
+                continue;
+            }
+            printf("%s", last_command);
+            strcpy(command, last_command);
+        } else {
+            strcpy(last_command, command);
+            has_history = true;
+        }
+        
+        // Parse the command
+        int num_args = parse_command(command, args);
+        if (num_args == 0) continue;
+        
+        // Check for exit command
+        if (strcmp(args[0], "exit") == 0) {
+            should_run = 0;
+            continue;
+        }
+        
+        // Check for background operation
+        bool background = false;
+        if (strcmp(args[num_args-1], "&") == 0) {
+            background = true;
+            args[num_args-1] = NULL;
+            num_args--;
+        }
+        
+        // Check for I/O redirection
+        char *input_file = NULL;
+        char *output_file = NULL;
+        for (int i = 0; i < num_args; i++) {
+            if (strcmp(args[i], "<") == 0) {
+                input_file = args[i + 1];
+                args[i] = NULL;
+                i++;
+            } else if (strcmp(args[i], ">") == 0) {
+                output_file = args[i + 1];
+                args[i] = NULL;
+                i++;
+            }
+        }
+        
+        // Fork and execute command
+        pid_t pid = fork();
+        
+        if (pid < 0) {
+            perror("Fork failed");
+            continue;
+        }
+        
+        if (pid == 0) { // Child process
+            // Handle input redirection
+            if (input_file != NULL) {
+                int fd = open(input_file, O_RDONLY);
+                if (fd == -1) {
+                    perror("Error opening input file");
+                    exit(1);
+                }
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+            }
+            
+            // Handle output redirection
+            if (output_file != NULL) {
+                int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (fd == -1) {
+                    perror("Error opening output file");
+                    exit(1);
+                }
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
+            }
+            
+            // Execute the command
+            if (execvp(args[0], args) == -1) {
+                perror("Error executing command");
+                exit(1);
+            }
+        } else { // Parent process
+            if (!background) {
+                waitpid(pid, NULL, 0);
+            }
+        }
     }
+    
     return 0;
 }
