461db7caffd5a9e76fd0b59b1122ab8f17567b81
Josue-B-Navarrete <90738503+Josue-B-Navarrete@users.noreply.github.com>
2025-04-17 14:08:17 -0700

    updated

--- assign4/buffer.cpp
+++ assign4/buffer.cpp
@@ -1,13 +1,128 @@
 /**
-* Assignment 4: Producer Consumer Problem
+ * Assignment 4: Producer Consumer Problem
  * @file buffer.cpp
  * @author ??? (TODO: your name)
  * @brief Implementation file for the buffer class
  * @version 0.1
  */
 
-// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
-#include "buffer.h"
-
-// TODO: Add your implementation of the buffer class here
+ #include "buffer.h"
+ #include <iostream>
+ 
+ /**
+  * @brief Construct a new Buffer object with the specified size
+  * @param bufferSize Size of the buffer (default is 5)
+  */
+ Buffer::Buffer(int bufferSize) {
+     size = bufferSize;
+     buff = new int[size];
+     front = 0;
+     rear = -1;
+     currentSize = 0;
+ }
+ 
+ /**
+  * @brief Destroy the Buffer object and free allocated memory
+  */
+ Buffer::~Buffer() {
+     delete[] buff;
+ }
+ 
+ /**
+  * @brief Insert an item into the buffer
+  * @param item The item to insert
+  * @return true if insertion successful, false if buffer is full
+  */
+ bool Buffer::insert_item(buffer_item item) {
+     // Check if buffer is full
+     if (is_full()) {
+         return false;
+     }
+     
+     // Update rear pointer and insert item
+     rear = (rear + 1) % size;
+     buff[rear] = item;
+     currentSize++;
+     return true;
+ }
+ 
+ /**
+  * @brief Remove an item from the buffer
+  * @param item Pointer to store the removed item
+  * @return true if removal successful, false if buffer is empty
+  */
+  bool Buffer::remove_item(buffer_item *item) {
+    // Check if buffer is empty
+    if (is_empty()) {
+        return false;
+    }
+    
+    // Get the item at the front position and store it in the provided pointer
+    *item = buff[front];
+    
+    // Update front pointer with wrap-around
+    front = (front + 1) % size;
+    currentSize--;
+    
+    // Handle the case when the buffer becomes empty
+    if (currentSize == 0) {
+        // You could reset pointers here if desired
+         front = 0;
+         rear = -1;
+    }
+    
+    return true;
+}
+ 
+ /**
+  * @brief Get the maximum size of the buffer
+  * @return The maximum size of the buffer
+  */
+ int Buffer::get_size() {
+     return size;
+ }
+ 
+ /**
+  * @brief Get the current number of items in the buffer
+  * @return The current number of items
+  */
+ int Buffer::get_count() {
+     return currentSize;
+ }
+ 
+ /**
+  * @brief Check if the buffer is empty
+  * @return true if empty, false otherwise
+  */
+ bool Buffer::is_empty() {
+     return currentSize == 0;
+ }
+ 
+ /**
+  * @brief Check if the buffer is full
+  * @return true if full, false otherwise
+  */
+ bool Buffer::is_full() {
+     return currentSize == size;
+ }
+ 
+ /**
+  * @brief Print the current contents of the buffer
+  */
+ void Buffer::print_buffer() {
+    if (is_empty()) {
+        std::cout << "Buffer: []" << std::endl;
+        return;
+    }
+    
+    std::cout << "Buffer: [";
+    int index = front;
+    for (int count = 0; count < currentSize; count++) {
+        std::cout << buff[index];
+        if (count < currentSize - 1) {
+            std::cout << ", ";
+        }
+        index = (index + 1) % size;
+    }
+    std::cout << "]" << std::endl;
+}
--- assign4/main.cpp
+++ assign4/main.cpp
@@ -1,65 +1,142 @@
 /**
-* Assignment 4: Producer Consumer Problem
+ * Assignment 4: Producer Consumer Problem
  * @file main.cpp
  * @author ??? (TODO: your name)
  * @brief The main program for the producer consumer problem.
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
-#include <iostream>
-#include "buffer.h"
-#include <unistd.h>
 
-using namespace std;
-
-// global buffer object
-Buffer buffer;
-
-// Producer thread function
-// TODO: Add your implementation of the producer thread here
-void *producer(void *param) {
-    // Each producer insert its own ID into the buffer
-    // For example, thread 1 will insert 1, thread 2 will insert 2, and so on.
-    buffer_item item = *((int *) param);
-
-    while (true) {
-        /* sleep for a random period of time */
-        usleep(rand()%1000000);
-        // TODO: Add synchronization code here
-        if (buffer.insert_item(item)) {
-            cout << "Producer " << item << ": Inserted item " << item << endl;
-            buffer.print_buffer();
-        } else {
-            cout << "Producer error condition"  << endl;    // shouldn't come here
-        }
-    }
-}
-
-// Consumer thread function
-// TODO: Add your implementation of the consumer thread here
-void *consumer(void *param) {
-    buffer_item item;
-
-    while (true) {
-        /* sleep for a random period of time */
-        usleep(rand() % 1000000);
-        // TODO: Add synchronization code here
-        if (buffer.remove_item(&item)) {
-            cout << "Consumer " << item << ": Removed item " << item << endl;
-            buffer.print_buffer();
-        } else {
-            cout << "Consumer error condition" << endl;    // shouldn't come here
-        }
-    }
-}
-
-int main(int argc, char *argv[]) {
-    /* TODO: 1. Get command line arguments argv[1],argv[2],argv[3] */
-    /* TODO: 2. Initialize buffer and synchronization primitives */
-    /* TODO: 3. Create producer thread(s).
-     * You should pass an unique int ID to each producer thread, starting from 1 to number of threads */
-    /* TODO: 4. Create consumer thread(s) */
-    /* TODO: 5. Main thread sleep */
-    /* TODO: 6. Exit */
-}
+ #include <iostream>
+ #include <pthread.h>
+ #include <unistd.h>
+ #include <mutex>
+ #include <condition_variable>
+ #include <cstdlib>
+ #include "buffer.h"
+ 
+ using namespace std;
+ 
+ // Global variables
+ Buffer* buffer;
+ mutex buffer_mutex;
+ condition_variable not_full;
+ condition_variable not_empty;
+ int sleep_time;
+ int num_producers;
+ int num_consumers;
+ 
+ /**
+  * @brief Producer thread function
+  * @param param Pointer to the producer ID
+  * @return void pointer (not used)
+  */
+ void* producer(void* param) {
+     // Each producer inserts its own ID into the buffer
+     int producer_id = *((int*)param);
+     buffer_item item = producer_id;
+     
+     while (true) {
+         // Sleep for a random period of time
+         usleep(rand() % 1000000);
+         
+         // Critical section - try to insert an item
+         unique_lock<mutex> lock(buffer_mutex);
+         
+         // Wait until buffer is not full
+         not_full.wait(lock, []{return !buffer->is_full();});
+         
+         if (buffer->insert_item(item)) {
+             cout << "Producer " << producer_id << ": Inserted item " << item << endl;
+             buffer->print_buffer();
+             
+             // Signal that buffer is not empty
+             not_empty.notify_one();
+         } else {
+             cout << "Producer error condition" << endl;  // shouldn't come here
+         }
+     }
+     
+     return nullptr;
+ }
+ 
+ /**
+  * @brief Consumer thread function
+  * @param param Pointer to the consumer ID (not used in output)
+  * @return void pointer (not used)
+  */
+ void* consumer(void* param) {
+     buffer_item item;
+     // Consumer ID is not used in the output format
+     
+     while (true) {
+         // Sleep for a random period of time
+         usleep(rand() % 1000000);
+         
+         // Critical section - try to remove an item
+         unique_lock<mutex> lock(buffer_mutex);
+         
+         // Wait until buffer is not empty
+         not_empty.wait(lock, []{return !buffer->is_empty();});
+         
+         if (buffer->remove_item(&item)) {
+             // Format matches expected output - no consumer ID
+             cout << "Consumer Removed item " << item << endl;
+             buffer->print_buffer();
+             
+             // Signal that buffer is not full
+             not_full.notify_one();
+         } else {
+             cout << "Consumer error condition" << endl;  // shouldn't come here
+         }
+     }
+     
+     return nullptr;
+ }
+ 
+ /**
+  * @brief Main function
+  * @param argc Argument count
+  * @param argv Argument vector
+  * @return 0 on successful execution
+  */
+ int main(int argc, char* argv[]) {
+     // 1. Get command line arguments
+     if (argc != 4) {
+         cout << "Usage: " << argv[0] << " <sleep time> <num producers> <num consumers>" << endl;
+         return 1;
+     }
+     
+     sleep_time = atoi(argv[1]);
+     num_producers = atoi(argv[2]);
+     num_consumers = atoi(argv[3]);
+     
+     // 2. Initialize buffer and seed random number generator
+     buffer = new Buffer(5);  // Create buffer with size 5
+     srand(time(nullptr));
+    
+     
+     // 3. Create producer thread(s)
+     pthread_t producer_threads[num_producers];
+     int producer_ids[num_producers];
+     
+     for (int i = 0; i < num_producers; i++) {
+         producer_ids[i] = i + 1;  // Producer IDs start from 1
+         pthread_create(&producer_threads[i], nullptr, producer, &producer_ids[i]);
+     }
+     
+     // 4. Create consumer thread(s)
+     pthread_t consumer_threads[num_consumers];
+     int consumer_ids[num_consumers];
+     
+     for (int i = 0; i < num_consumers; i++) {
+         consumer_ids[i] = i + 1;  // Consumer IDs start from 1
+         pthread_create(&consumer_threads[i], nullptr, consumer, &consumer_ids[i]);
+     }
+     
+     // 5. Main thread sleep
+     sleep(sleep_time);
+     
+     
+     delete buffer;
+     return 0;
+ }
